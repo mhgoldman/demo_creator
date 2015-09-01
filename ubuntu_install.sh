cf89c518ab69d7d94c819b05e169a3367288c383
Martin Goldman <mgoldman@skytap.com>
2015-09-01 14:57:15 -0400

    add to script

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -3,12 +3,21 @@
 # Install script - Tested on Ubuntu 14.04 ONLY
 set -e 
 
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libgdbm-dev libncurses5-dev automake libtool bison libffi-dev libgmp-dev postgresql libpq-dev nodejs libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo -y apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libgdbm-dev libncurses5-dev automake libtool bison libffi-dev libgmp-dev postgresql libpq-dev nodejs libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+# DON'T PROMPT!
 
 sudo -u postgres createuser `whoami` -s
 
+command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 curl -L https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
+if [ -f ~/.rvm/scripts/rvm ]; then
+  source ~/.rvm/scripts/rvm
+else
+  source /etc/profile.d/rvm.sh
+fi
+
 rvm install 2.2.3
 gem install bundler
 
